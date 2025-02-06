428e003b82a087dcaa3738064ed2a6a1abef730d
Aksh-Agg <aksh.aggarwal@shell.com>
2025-02-06 16:01:10 +0530

    comments removed

--- app/main.py
+++ app/main.py
@@ -11,29 +11,16 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-origins = ["*"]  # Define your allowed origins
+origins = ["*"]  
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE"],  # List of HTTP methods
-    allow_headers=origins,  # List of allowed headers
+    allow_methods=["GET", "POST", "PUT", "DELETE"],  
+    allow_headers=origins,  
 )
     
-# try:
-#     conn = psycopg2.connect(host='localhost', database='postgres', user='postgres', password='Aksh!@#$', cursor_factory=RealDictCursor)
-#     cursor= conn.cursor()
-#     print("Connected to the database")
-#     cursor = conn.cursor()
-#     cursor.execute("SHOW port;")
-#     port = cursor.fetchone()[0]
-#     print(f"PostgreSQL is running on port: {port}")
-
-# except Exception as e:
-#     print(f"An error occurred: {e}")
-#     conn = None
-
 
 app.include_router(posts.router)
 app.include_router(users.router)
